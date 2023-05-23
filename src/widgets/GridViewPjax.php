8a348d993e3629d542e28de57fc6762c06ea7b00
fabian-dz <fabian@dezero.es>
2023-05-23 16:02:47 +0200

    GridviewPjax - Solving PSR-4 autoloading standard

---
+++ src/widgets/GridViewPjax.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @author Fabián Ruiz <fabian@dezero.es>
+ * @link http://www.dezero.es
+ * @copyright Copyright &copy; 2023 Fabián Ruiz
+ */
+
+namespace dezero\widgets;
+
+use Yii;
+
+/**
+ * Pjax is a widget integrating the [pjax](https://github.com/yiisoft/jquery-pjax) jQuery plugin.
+ */
+class GridViewPjax extends \yii\widgets\Pjax
+{
+    /**
+     * @var string The GridView id
+     */
+    public $gridview;
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function init()
+    {
+        $this->options['class'] = 'dz-grid-container';
+        if ( !empty($this->gridview) )
+        {
+            $this->options['id'] = $this->gridview .'-container';
+        }
+
+        parent::init();
+    }
+}
