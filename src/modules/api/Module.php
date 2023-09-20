99114db6eab986ee83327a37ee6b1d982894c7b4
fabian-dz <fabian@dezero.es>
2023-09-20 07:35:36 +0200

    API module - Adding CORE controller into controllerMap

--- src/modules/api/Module.php
+++ src/modules/api/Module.php
@@ -11,6 +11,8 @@ class Module extends \dezero\base\Module
      * @var array mapping from controller ID to controller configurations.
      */
     public $controllerMap = [
+        'assets'   => \dezero\modules\api\controllers\AssetsController::class,
+        'auth'     => \dezero\modules\api\controllers\AuthController::class,
         'users'     => \dezero\modules\api\controllers\UsersController::class
     ];
 
